b9b9af58b04b23f7b80a4a14a252a8798857451e
MOISES MARTINEZ CID <20183l301006@utcv.edu.mx>
2021-06-29 18:07:45 -0500

    Se agrego el pipe de pares y se modifico el slideshow pares

---
+++ src/app/components/detalle/detalle.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Cast, PeliculaDetalle } from 'src/app/interfaces/interface';
+import { MoviesService } from 'src/app/services/movies.service';
+
+@Component({
+  selector: 'app-detalle',
+  templateUrl: './detalle.component.html',
+  styleUrls: ['./detalle.component.scss'],
+})
+export class DetalleComponent implements OnInit {
+  @Input() id;
+
+  pelicula: PeliculaDetalle = {};
+  actores: Cast[] = [];
+  oculto = 150;
+  estrella = 'star-outline';
+
+
+
+  constructor(private moviesService: MoviesService,
+               private modalCtrl: ModalController,) { }
+
+  ngOnInit() {
+    this.moviesService.getPeliculaDetalle( this.id )
+    .subscribe( resp => {
+      console.log( resp );
+      this.pelicula = resp;
+    });
+
+    this.moviesService.getActoresPelicula( this.id )
+    .subscribe( resp => {
+      console.log( resp );
+      this.actores = resp.cast;
+    });
+
+  }
+  regresar() {
+    this.modalCtrl.dismiss();
+  }
+
+}
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { MDBResponse } from '../interfaces/interface';
+import { Genre, MDBResponse, PeliculaDetalle, RespuestaCredits } from '../interfaces/interface';
 const URL    = environment.url;
 const apiKey = environment.apiKey;
 
@@ -10,6 +10,7 @@ const apiKey = environment.apiKey;
 })
 export class MoviesService {
   private popularesPage = 0;
+  generos: Genre[] = [];
 
   constructor(private http : HttpClient) { }
 
@@ -44,4 +45,28 @@ getPopulares() {
   return this.ejecutarQuery<MDBResponse>(query);
 
 }
+getPeliculaDetalle( id: string ) {
+  return this.ejecutarQuery<PeliculaDetalle>(`/movie/${ id }?a=1`);
+}
+
+getActoresPelicula( id: string ) {
+  return this.ejecutarQuery<RespuestaCredits>(`/movie/${ id }/credits?a=1`);
+}
+
+cargarGeneros(): Promise<Genre[]> {
+
+  return new Promise( resolve => {
+
+    this.ejecutarQuery(`/genre/movie/list?a=1`)
+      .subscribe( resp => {
+        this.generos = resp['genres'];
+        console.log(this.generos);
+        resolve(this.generos);
+      });
+
+  });
+
+
+}
+
 }
